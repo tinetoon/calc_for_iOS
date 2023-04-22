5c49d77dedbcb8cf95e264bf8ee545668533715a
Dyuzhakov Anton <tinetoon@mail.ru>
2023-04-22 07:45:00 +0300

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Anton Dyuzhakov on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
