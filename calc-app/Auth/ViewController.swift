3ec84009431f45b8f2dd5a117b617e50378a0e77
Dyuzhakov Anton <tinetoon@mail.ru>
2023-04-30 10:23:34 +0300

    Add keys 1, 2, 3, 4

--- calc-app/Auth/ViewController.swift
+++ calc-app/Auth/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet var mainLabel: UILabel!
     // Элемент кнопки
     @IBOutlet var setOneButton: UIButton!
+    @IBOutlet var setTwoButton: UIButton!
+    @IBOutlet var setThreeButton: UIButton!
+    @IBOutlet var setSubButton: UIButton!
     // Элемент окна вывода
     @IBOutlet var inputLabel: UILabel!
 
@@ -23,23 +26,61 @@ class ViewController: UIViewController {
         // Пользовательские тексты для элементов
         mainLabel.text = "Calculator"
         inputLabel.text = ""
-        setOneButton.setTitle("[Enter]", for: .normal) // Обычное состояние
-        setOneButton.setTitle("[OK]", for: .highlighted) // Нажатая кнопка
+        setOneButton.setTitle("[1]", for: .normal) // Обычное состояние
+        setTwoButton.setTitle("[2]", for: .normal)
+        setThreeButton.setTitle("[3]", for: .normal)
+        setSubButton.setTitle("[+]", for: .normal)
+//        setOneButton.setTitle("[OK]", for: .highlighted) // Нажатая кнопка
         
         // Вызов метода, при нажатии кнопки
-        setOneButton.addTarget(self, action: #selector(didTapGetEnterButton), for: .touchUpInside)
+        setOneButton.addTarget(self, action: #selector(didTapGetOneButton), for: .touchUpInside)
+        setTwoButton.addTarget(self, action: #selector(didTapGetTwoButton), for: .touchUpInside)
+        setThreeButton.addTarget(self, action: #selector(didTapGetThreeButton), for: .touchUpInside)
+        setSubButton.addTarget(self, action: #selector(didTapGetSubButton), for: .touchUpInside)
         // terget=self - нажатие от кнопки приходит в текущий ViewController
         // Selector=#selector - (название метода)
         // action=touchUpInside - нажатие на кнопку и затем релиз кнопки (нажали/отпустили)
     }
     
     // Метод выполняемый по нажатии кнопки
-    @objc func didTapGetEnterButton() {
+    @objc func didTapGetOneButton() {
         
-        inputLabel.text = "Tapped Button Enter"
-        print("[INFO Log] Tapped Button Enter & displaying information on the screen")
+        setNumOnScreen(num: 1)
+        logTapOnButton(nameButton: "One")
     }
-
+    @objc func didTapGetTwoButton() {
+        
+        setNumOnScreen(num: 2)
+        logTapOnButton(nameButton: "Two")
+    }
+    @objc func didTapGetThreeButton() {
+        
+        setNumOnScreen(num: 3)
+        logTapOnButton(nameButton: "Three")
+    }
+    @objc func didTapGetSubButton() {
+        
+        setStringOnScreen(stringButton: "+")
+        logTapOnButton(nameButton: "Sub")
+    }
+    
+    // Метод выводящий на экран число по нажатию кнопки
+    func setNumOnScreen(num: Int) {
+        
+        inputLabel.text = String(num)
+    }
+    
+    // Метод выводящий на экран символ по нажатию кнопки
+    func setStringOnScreen(stringButton: String) {
+        
+        inputLabel.text = stringButton
+    }
+    
+    // Логирование нажатия кнопки
+    func logTapOnButton(nameButton: String) {
+        
+        print("[INFO Log] Tapped " + nameButton + " Button & displaying information on the screen")
+    }
 
 }
 
